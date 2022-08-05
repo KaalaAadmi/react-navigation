6852224546dc2ba10118c164b82837928dfba27d
KaalaAadmi <arnav264@gmail.com>
2022-08-05 17:43:45 +0530

    react-navigation done

--- App.js
+++ App.js
@@ -1,15 +1,62 @@
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Detail Screen"
+        onPress={() => navigation.navigate("Details")}
+      />
+    </View>
+  );
+}
+function DetailScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Detail Screen</Text>
+      <Button
+        title="Go to Detail Screen Again"
+        onPress={() => navigation.push("Details")}
+      />
+      <Button
+        title="Go to Home Screen"
+        onPress={() => navigation.navigate("Home")}
+      />
+      <Button title="Go Back" onPress={() => navigation.goBack()} />
+      <Button
+        title="Go to First Screen"
+        onPress={() => navigation.popToTop()}
+      />
+    </View>
+  );
+}
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#009378",
+          },
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+        }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "Overview" }}
+        />
+        <Stack.Screen name="Details" component={DetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
